8f90d9859a0b1ad6dceaaed00a903f364ffe1b86
waseemusman17 <waseem.usman17@gmail.com>
2022-08-23 07:05:57 +0500

    Top Main Section

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+
+$('.featured--slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    autoplay: true,
+    // slideTransition: 'linear',
+    // autoplaySpeed: 3000,
+    smartSpeed: 500,
+    items:1,
+
+
+});
