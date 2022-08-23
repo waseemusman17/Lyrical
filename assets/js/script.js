011654c0d89e5d971ba6bfddd08fdf1d3397da66
waseemusman17 <waseem.usman17@gmail.com>
2022-08-23 08:01:17 +0500

    Home Add new Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,28 @@ $('.featured--slider').owlCarousel({
     // autoplaySpeed: 3000,
     smartSpeed: 500,
     items:1,
+});
 
+$('.artist--slider').owlCarousel({
+    loop:true,
+    margin:40,
+    nav:false,
+    dots:false,
+    autoplay: true,
+    // slideTransition: 'linear',
+    // autoplaySpeed: 3000,
+    smartSpeed: 500,
+    items:4,
+});
 
+$('.latest--release_slider').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    dots:false,
+    autoplay: true,
+    // slideTransition: 'linear',
+    // autoplaySpeed: 3000,
+    smartSpeed: 500,
+    items:4,
 });
